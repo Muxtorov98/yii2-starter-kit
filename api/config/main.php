455ae22863c50589cac3cf598ce8219009b7761a
Tulqin_Muxtorov98 <104984928+Muxtorov98@users.noreply.github.com>
2022-11-30 10:35:39 +0500

    api

---
+++ api/config/main.php
@@ -0,0 +1,61 @@
+<?php
+require(__DIR__ . '/../../common/components/helpers/GlobalFunctions.php');
+$params = array_merge(
+    require __DIR__ . '/../../common/config/params.php',
+    require __DIR__ . '/../../common/config/params-local.php',
+    require __DIR__ . '/params.php',
+    require __DIR__ . '/params-local.php'
+);
+
+
+return [
+    'id' => 'app-api',
+    'name' => 'Starter Kit - Admin Panel',
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'api\controllers',
+    'bootstrap' => ['log'],
+    'modules' => [],
+    'components' => [
+        'request' => [
+            'baseUrl' => '/api',
+            'csrfParam' => '_csrf-backend',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+                'multipart/form-data' => 'yii\web\MultipartFormDataParser'
+            ]
+        ],
+        'response' => [
+            'format' =>  \yii\web\Response::FORMAT_JSON
+        ],
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+            'enableSession' => true,
+            'authTimeout' => 2 * 60 * 60,
+            'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
+        ],
+        'session' => [
+            // this is the name of the session cookie used for login on the backend
+            'name' => 'advanced-backend',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'backendUrlManager' => require __DIR__ . '/UrlManager.php',
+        'urlManager' => function () {
+            return Yii::$app->get('backendUrlManager');
+        },
+    ], // components
+
+
+    'params' => $params,
+];
